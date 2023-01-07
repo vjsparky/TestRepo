d36665d05d0626bb56a0a06faa6695e4b91790ab
vjsparky <vijayshanmugam6298@gmail.com>
2023-01-07 12:14:01 +0530

    sqlchange

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(host="localhost", user="root", password="Sparky@6@21998")
-mycursor = mydb.cursor()
-mycursor.execute("select * from vijay.sftpaudit;")
-result = mycursor.fetchall()
-for i in result:
-  print (i)
+def DBTableupdate(Filename, status, Submitdate):
+    try : 
+        mydb = mysql.connector.connect(host="localhost", user="root", password="Sparky@6@21998")
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO vijay.sftpaudit (Filename, status, Submitdate ) VALUES (%s, %s, %s)"
+        val = (Filename, status, Submitdate)
+        mycursor.execute(sql, val)
+        mydb.commit()
+        print("Successfully updated in the DB Table ")
+    except Exception as e :
+        print("Unable to update in the DB Table..", e)
+
+DBTableupdate("Filename", "Sucess", "2022-12-01")
+DBTableupdate("Filename", "Failed", "2022-12-01")
